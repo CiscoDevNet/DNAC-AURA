80dcd00326820d3de793a7a7a344bef50cdbd226
Adam Radford <maglev@maglev-master-10-66-104-121>
2020-09-09 03:23:21 +0000

    Version:1.2.4

---
+++ run_remote/README.md
@@ -0,0 +1,101 @@
+# Remote execution of AURA
+This script allows you to lanch the AURA on a remote Cisco DNA Center.  It uses paramiko and scp libraries.
+
+## Installing
+To install, it is recommended that you use a virtual environment.  The following lines will create a python3 virtual 
+environment, activate it, upgrade pip, and install the required libraaries
+
+```buildoutcfg
+python3 -m venv env3
+source env3/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+## Note about remote execution (session timeout)
+Later versions (2.1+) of Cisco DNA Center have an ssh idle timeout.  This can impact AURA being run from an ssh session
+either directly on DNAC, or indirectly via the run_remote script or ansible.
+
+The work around is simple.  For an ssh connection, the "-o ServerAliveInterval=3" flag will send keepalives
+and maintain the session.  This is used in this script, and can also be used for direct ssh connection as well as 
+ansible
+
+## Using the script
+The script requires three arguments:
+- dnac
+- admin password (also available as an environment variable DNAC_ADMIN_PASS)
+- maglev password (also available as an environment variable DNAC_MAGLEV_PASS)
+
+the simplest way to run the script with arguments (see later section on environment vars) is
+```buildoutcfg
+./run_remote.py --dnac 1.1.1.1 --admin-pass passwd --maglev-pass passwd
+```
+
+If you are familiar with shell environment variables, this can be simplified further
+
+```buildoutcfg
+export DNAC_ADMIN_PASS="passwd"
+export DNAC_MAGLEV_PASS="passwd"
+./run_remote.py --dnac 10.1.1.1
+```
+
+## Passing AURA options (--)
+To pass AURA specific arguments (for example -s to run SDA tests) you need to do the following:
+```buildoutcfg
+## note the extra --, due to a qwirk in the way argparse library works
+./run_remote.py --dnac 10.1.1.1 -- -s
+```
+
+## Storing aura output locally.
+AURA script supports the --json-summaary option. This produces a json summary of the test results as well as the 
+location of the report and log file on DNAC.  When run_remote is supplied with the --local-dir option, the 
+log and report files will be moved back to DNAC.  A json-summary file will be created.
+A directory for the DNAC is created
+
+```buildoutcfg
+/home/aradford/RUN_REMOTE/run_remote.py --dnac 10.1.1.1  --local-dir /home/aradford/RUN_REMOTE/logs
+```
+after this completes, the /home/aradford/RUN_REMOTE/logs directory will contain:
+```buildoutcfg
+ls RUN_REMOTE/logs/10.1.1.1
+DNAC_AURA_Logs_2020-09-08_23_20_11.tar.gz
+DNAC_AURA_Report_2020-09-08_23_20_11.json
+DNAC_AURA_Report_2020-09-08_23_20_11.pdf
+```
+
+the json file contains:
+```buildoutcfg
+cat RUN_REMOTE/logs/*/DNAC_AURA_Report_2020-09-08_23_20_11.json
+{
+  "json-summary": {
+    "check_count": 64,
+    "report-name": "/data/tmp/dnac_aura/reports/DNAC_AURA_Report_2020-09-08_23_20_11.pdf",
+    "logfile-name": "/data/tmp/dnac_aura/logs/DNAC_AURA_Logs_2020-09-08_23_20_11.tar.gz",
+    "ur_check_count": 19,
+    "ur_error_count": 0,
+    "warning_count": 5,
+    "assur_warning_count": 2,
+    "error_count": 5,
+    "ur_warning_count": 3,
+    "assur_check_count": 14,
+    "assur_error_count": 0
+  }
+}
+```
+
+## Other options
+The script can also be run with the --no-pull option.  This stops the git pull to update to the 
+latest version of AURA, but assumes you have copied aura to the home directory on DNA Center.
+
+## CRON
+Cron is a challenge for AURA due to the lack of PTY. It also requires editing the DNA Center crontab.
+
+run_remote probably a better way of running AURA, as it solves the PTY issue and removes the need to edit the local 
+DNA Center crontab.  Running remotely combined with --local-path means all DNA Center logs are in the same on an external
+server.
+
+Here is a sample crontab entry for running AURA on a DNAC every hour.
+Need to supply the python interpreter explicitly to pick up the virtual environment contain paramiko and scp libraries.
+
+```buildoutcfg
+00 * * * * /home/aradford/RUN_REMOTE/env3/bin/python /home/aradford/RUN_REMOTE/run_remote.py --dnac 10.1.1.1 --admin-pass passwd --maglev-pass passwd --local-dir /home/aradford/RUN_REMOTE/logs > /tmp/run
+```
---
+++ run_remote/run_remote.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from paramiko import  SSHClient, AutoAddPolicy,AuthenticationException
+from scp import SCPClient
+import json
+import os
+from argparse import ArgumentParser, REMAINDER
+
+def connect(ip, password):
+    try:
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(AutoAddPolicy())
+
+        print('Initiating SSH to  {} ...'.format(ip))
+        ssh.connect(ip, port=2222, username="maglev", password=password ,look_for_keys=False)
+
+        # use keepalive to keep connection open to DNAC with idle timeout=5mins
+        t = ssh.get_transport()
+        t.set_keepalive(5)
+        return ssh
+    except AuthenticationException:
+        print("Unable to SSH to the device {} due to an Authentication Exception".format(ip))
+
+def validate_op(stdout, stderr):
+    stderr._set_mode('rb')
+    status = stderr.read().decode('ascii').strip("\n")
+    if not status:
+        stdout._set_mode('rb')
+        opstatus = stdout.read().decode('ascii').strip("\n")
+        print(opstatus)
+    else:
+        print(status)
+
+def copy_files(conn, dnac, json_summary, dir):
+    path = "{}/{}".format(dir, dnac)
+    print("\n**** Checking local destination folder: {}".format(path))
+    if not os.path.isdir(path):
+        try:
+            os.mkdir(path)
+        except OSError:
+            print("Creation of the directory %s failed" % path)
+        else:
+            print("Successfully created the directory %s " % path)
+
+    print("\n**** Copy logfiles and report files back to {}".format(path))
+    scp = SCPClient(conn.get_transport())
+    scp.get(json_summary['json-summary']['report-name'], local_path=path)
+    scp.get(json_summary['json-summary']['logfile-name'], local_path=path)
+
+def main(dnac, maglev, admin, nopull, dir, rest):
+    #print (dnac, maglev, admin, rest)
+    others = ''
+    if rest != []:
+        others = ' '.join(rest[1:])
+
+
+    with connect(dnac, maglev) as conn:
+
+        if not nopull :
+            print("\nCollecting proxy info...")
+            try:
+                stdin, stdout, stderr = conn.exec_command("cat /etc/systemd/system/docker.service.d/http*-proxy.conf | awk '/HTTPS_PROXY/ {print}'")
+                stdout._set_mode('rb')
+                proxy = stdout.read().decode('ascii').strip("\n")
+                proxy = proxy.split("=")[2]
+            except IndexError:
+                print("no proxy found")
+                proxy = ''
+
+            print("\nCloning/Pulling latest AURA Code...\n")
+            stdin, stdout, stderr= conn.exec_command("https_proxy={} git clone https://github.com/CiscoDevNet/DNAC-AURA.git".format(proxy))
+            validate_op(stdout, stderr)
+            stdin, stdout, stderr = conn.exec_command("cd DNAC-AURA; git pull")
+            validate_op(stdout, stderr)
+
+        json_summary = {}
+        print("\n\n****** EXECUTING AURA ON NODE : {} ******\n\n".format(dnac))
+        json_flag = "--json-summary" if dir is not None else ""
+        cmd = "/home/maglev/DNAC-AURA/dnac_aura --admin-pass {} --maglev-pass {} {} {}".format(admin,maglev, json_flag, others)
+        stdin, stdout, stderr = conn.exec_command(cmd,get_pty=True)
+        while True:
+            line = stdout.readline()
+            print(line, end='')
+            if "json-summary" in line:
+                json_summary = json.loads(line)
+            if not line: break
+        #print(json.dumps(json_summary, indent=2))
+
+        if dir is not None:
+            if json_summary == {}:
+                print("No json summary cannot copy logfiles and report")
+                return
+
+            copy_files(conn, dnac, json_summary, dir)
+
+            # copy a summary
+            print("\n******* WRITING JSON SUMMARYS ******")
+            base = os.path.splitext(os.path.basename(json_summary['json-summary']['report-name']))[0]
+            with open("{}/{}/{}.json".format(dir, dnac,base), "w") as f:
+                json.dump(json_summary, f, indent=2)
+
+            ### cleaning up remote files
+            print("\n******* CLEANING IP REMOTE REPORT AND LOGFILES ******")
+            cmd = "rm {} {}".format(json_summary['json-summary']['report-name'], json_summary['json-summary']['logfile-name'])
+            stdin, stdout, stderr = conn.exec_command(cmd)
+            validate_op(stdout, stderr)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Select options.')
+    parser.add_argument('--dnac', type=str, required=True,
+                        help="dnac to connect to")
+    parser.add_argument('--maglev-pass', type=str, default=os.environ.get('DNAC_MAGLEV_PASS','password'),
+                        help="maglev password")
+    parser.add_argument('--admin-pass', type=str,default=os.environ.get('DNAC_ADMIN_PASS','password'),
+                        help="admin (WEBUI) password")
+    parser.add_argument('--no-pull', action='store_true', default=False,
+                        help="do not pull down new copy of aura")
+    parser.add_argument('--local-dir', type=str,
+                        help="local directory to store report/logs")
+    parser.add_argument('rest', nargs=REMAINDER)
+    args = parser.parse_args()
+
+    if args.maglev_pass == "password":
+        print("\n** Warning, using default maglev password, use DNAC_MAGLEV_PASS environment var or --maglev-pass")
+    if args.maglev_pass == "password":
+        print("\n** Warning, using default admin password, use DNAC_ADMIN_PASS environment var or --admin-pass")
+
+    main(args.dnac, args.maglev_pass, args.admin_pass, args.no_pull, args.local_dir, args.rest)
